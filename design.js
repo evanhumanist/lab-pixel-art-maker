6bee3b0b35d51666489aad96d59f4d9ad9d82106
Evan Miller <evanhumanist@gmail.com>
2018-05-29 22:14:06 -0400

    feat: Added selectors to select elements by Class Name

--- design.js
+++ design.js
@@ -3,6 +3,14 @@
 
 // When size is submitted by the user, call makeGrid()
 
+//Define Selectors
+const inputHeight = document.getElementsByClassName('input-height');
+const inputWidth = document.getElementsByClassName('input-width');
+const submitButton = document.getElementsByClassName('submit-button');
+const colorPicker = document.getElementsByClassName('color-picker');
+const pixelCanvas = document.getElementsByClassName('pixel-canvas');
+
+
 function makeGrid() {
   // Your code goes here!
 }
